4547a86a2c554c3ea5ef6cbdb3557e7db8ca0a28
Ivan Preziosi <ivan.preziosi@gmail.com>
2022-02-14 03:25:30 +0100

    +

--- config/gastropod.php
+++ config/gastropod.php
@@ -4,13 +4,12 @@ return [
     /**
      * Set to false to leave the crud open to the public!
      */
-    'enable_gastropod_auth' => true,
-    
+    'enable_gastropod_auth' => true,    
     
     /**
      * Gastropod resources to build admin routes
      */
-    'resurces' => [
+    'resources' => [
         'users' => 'RadFic\Gastropod\Http\Controllers\UserCrudController',
         'admins' => 'RadFic\Gastropod\Http\Controllers\AdminCrudController',
     ]
