a68391185123fd996978dc63b3e593eac3e90e13
Ivan Preziosi <ivan.preziosi@gmail.com>
2022-02-15 22:49:41 +0100

    +

--- routes/gastropod.php
+++ routes/gastropod.php
@@ -4,16 +4,18 @@ use RadFic\Gastropod\Http\Controllers\GastropodController;
 use RadFic\Gastropod\Http\Controllers\UserCrudController;
 use RadFic\Gastropod\Http\Controllers\GastropodAdminCrudController;
 
+Route::get('/gastropod', [GastropodController::class,'getLogin']);
 Route::prefix('gastropod')->middleware(['web'])->group(function () {
-    Route::get('/', [GastropodController::class,'getLogin']);
-    Route::get('', [GastropodController::class,'getLogin']);
     Route::get('/login', [GastropodController::class,'getLogin']);
     Route::post('/login', [GastropodController::class,'doLogin']);
     Route::get('/logout', [GastropodController::class,'logout']);
-
     Route::middleware(['gastropodAuth'])->group(function () {
         /**
-         * Default routes: admin and users
+         * Map here your resources to the appropriate controller
+         * extending RadFic\Gastropod\Http\Controllers\BaseCrudTableController
+         * 
+         * Default routes: admin and users are precreated. Feel free to 
+         * inspect them and modify them to suit your needs.
          */
         Route::resources([
             'users' => 'RadFic\Gastropod\Http\Controllers\UserCrudController',
