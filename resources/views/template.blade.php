36762b9a33dacdd7cea182e616e94562b1b1d29a
Ivan Preziosi <ivan.preziosi@gmail.com>
2022-02-20 20:00:00 +0100

    ++

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -31,7 +31,7 @@
 				<div class="collapse navbar-collapse" id="navbarCollapse">
 					<ul class="navbar-nav me-auto mb-2 mb-md-0">
 						<li class="nav-item">
-							<a class="nav-link" aria-current="page" href="/gastropod/users">Users</a>
+							<a class="nav-link" aria-current="page" href="#">Users</a>
 						</li>
 						<li class="nav-item">
 							<a class="nav-link" aria-current="page" href="#">Another Section</a>
--- routes/gastropod.php
+++ routes/gastropod.php
@@ -2,14 +2,21 @@
 use Illuminate\Support\Facades\Route;
 use RadFic\Gastropod\Http\Controllers\GastropodController;
 
+/** 
+ * Gastropod Routes
+ */
 Route::middleware('web')->group(function () {
+	/** service routes */
     Route::get('/', [GastropodController::class,'getLogin']);
     Route::get('/login', [GastropodController::class,'getLogin']);
     Route::post('/login', [GastropodController::class,'doLogin']);
     Route::get('/logout', [GastropodController::class,'logout']);
 
+	/** resource routes */
 	Route::resources([
-        'users' => 'App\Http\Controllers\Gastropod\UserGastropodController',
+		/**
+		 * gastropod_admins is installed by default: it manages the crud admin permissions on app users. 
+		 */
         'gastropod_admins' => 'RadFic\Gastropod\Http\Controllers\GastropodAdminController'
     ]);
 
