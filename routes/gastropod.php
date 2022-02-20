5d112c5e54a50cb7265054f561e7003c2dd80fff
Ivan Preziosi <ivan.preziosi@gmail.com>
2022-02-20 14:59:56 +0100

    ++

--- routes/gastropod.php
+++ routes/gastropod.php
@@ -2,7 +2,9 @@
 use Illuminate\Support\Facades\Route;
 use RadFic\Gastropod\Http\Controllers\GastropodController;
 
-Route::get('/gastropod', [GastropodController::class,'getLogin']);
-Route::get('/gastropod/login', [GastropodController::class,'getLogin']);
-Route::post('/gastropod/login', [GastropodController::class,'doLogin']);
-Route::get('/gastropod/logout', [GastropodController::class,'logout']);
+Route::prefix('gastropod')->middleware('web')->group(function () {
+    Route::get('/', [GastropodController::class,'getLogin']);
+    Route::get('/login', [GastropodController::class,'getLogin']);
+    Route::post('/login', [GastropodController::class,'doLogin']);
+    Route::get('/logout', [GastropodController::class,'logout']);
+});
