7348676bb56c706a7abc6e7d71eb91c838d5a60f
Ivan Preziosi <ivan.preziosi@gmail.com>
2022-02-21 12:01:28 +0100

    ++

--- src/Console/CreateGastropodController.php
+++ src/Console/CreateGastropodController.php
@@ -3,7 +3,7 @@
 namespace RadFic\Gastropod\Console;
 
 use Illuminate\Console\GeneratorCommand;
-use Illuminate\Support\Facades\File;
+use Symfony\Component\Console\Input\InputArgument;
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\App;
@@ -11,11 +11,10 @@ use Illuminate\Filesystem\Filesystem;
 
 class CreateGastropodController extends GeneratorCommand
 {
-    protected $name = 'gastropod:controller';
+    protected $name = 'make:gastropodController';
     protected $description = 'Create a new Gastropod Controller in your App.';
     protected $type = 'GastropodCrudController';
-    protected $signature = 	'gastropod:controller {model}';
-    protected $stub = __DIR__ ."/../../stubs/gastropod.controller.stub";
+    protected $signature = 	'make:gastropodController {name}';
 
     protected function getDefaultNamespace($rootNamespace)
     {
@@ -24,19 +23,26 @@ class CreateGastropodController extends GeneratorCommand
 
     protected function getStub()
     {
-        return $stub;
+        return __DIR__ ."/../../stubs/gastropod.controller.stub";
+    }
+    
+    protected function getArguments()
+    {
+        return [
+            ['name', InputArgument::REQUIRED, 'The name of the gastropod controller.'],
+        ];
     }
 
     public function handle()
     {
         parent::handle();
-        $this->doOtherOperations();
+        //$this->doOtherOperations();
     }
 
     protected function doOtherOperations()
     {
         // Get the fully qualified class name (FQN)
-        $class = $this->qualifyClass($this->getNameInput());
+        $class = $this->argument($model);
 
         // get the destination path, based on the default namespace
         $path = $this->getPath($class);
